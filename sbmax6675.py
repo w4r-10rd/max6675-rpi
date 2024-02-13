841f51b8cfcfb0d3218b1f2cd550887617adf69a
Arijit Nath <111447922+w4r-10rd@users.noreply.github.com>
2024-02-13 19:36:22 +0530

    Update sbmax6675.py

--- sbmax6675.py
+++ sbmax6675.py
@@ -5,7 +5,7 @@ def on_message(client, userdata, message):
     print("Received message:", str(message.payload.decode("utf-8")))
 
 # MQTT broker and topic information
-broker_address = "192.168.0.131"  # Use the test.mosquitto.org MQTT broker
+broker_address = "test.mosquitto.org"  # Use the test.mosquitto.org MQTT broker
 port = 1883  # Default MQTT port
 topic = "temperaturepi3b/sensor_data/actual_temperature"
 
